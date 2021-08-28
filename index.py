d30f9f9deeb45906c8e0d93eafeec3bffb10a366
Ryan <ryanpivovar@gmail.com>
2021-08-28 15:00:56 -0400

    add initial notes for a number system as it relates to a 12-tone scale

--- index.py
+++ index.py
@@ -1,3 +1,8 @@
+# musical notes
+# A A# B C C# D D# E F F# G  G#
+# 0 1  2 3 4  5 6  7 8 9  10 11
+# this number system code be extended for octaves
+
 def generate_melody():
     melody = [0, 0, 0, 0, 0, 0, 0, 0]
     return melody
