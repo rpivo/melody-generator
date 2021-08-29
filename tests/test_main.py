e313d637ef9f197af09f8f445a83d26006cac74d
Ryan <ryanpivovar@gmail.com>
2021-08-28 22:32:18 -0400

    generate_melody returns a list containing eight numbers

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,3 +4,8 @@ from src.main import generate_melody
 def test_generate_melody_returns_a_list():
     result = generate_melody()
     assert isinstance(result, list)
+
+
+def test_generate_melody_returns_a_list_containing_eight_numbers():
+    result = generate_melody()
+    assert len(result) == 8
