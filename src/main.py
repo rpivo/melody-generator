2354e86043aecbe1484c352d321559c7b12b27ea
Ryan <ryanpivovar@gmail.com>
2021-08-28 22:38:33 -0400

    get_random_number_between_one_and_eight returns an integer

--- src/main.py
+++ src/main.py
@@ -3,9 +3,13 @@
 # 0 1  2 3 4  5 6  7 8 9  10 11
 # this number system code be extended for octaves
 
+from random import randint
+
+
 def generate_melody():
     melody = [0, 0, 0, 0, 0, 0, 0, 0]
     return melody
 
 
-print(generate_melody())
+def get_random_number_between_one_and_eight():
+    return randint(1, 8)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,4 @@
-from src.main import generate_melody
+from src.main import generate_melody, get_random_number_between_one_and_eight
 
 
 def test_generate_melody_returns_a_list():
@@ -9,3 +9,8 @@ def test_generate_melody_returns_a_list():
 def test_generate_melody_returns_a_list_containing_eight_numbers():
     result = generate_melody()
     assert len(result) == 8
+
+
+def test_get_random_number_between_one_and_eight_returns_an_integer():
+    result = get_random_number_between_one_and_eight()
+    assert type(result) is int
